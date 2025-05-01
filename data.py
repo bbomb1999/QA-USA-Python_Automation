5a3e69cd9d2334c8b0c8a0431495260c45419a30
bbomb1999 <benjaminhorowitz43@gmail.com>
2025-05-01 14:20:01 -0700

    Task 1 & 2 of Project

--- data.py
+++ data.py
@@ -1,1 +1,7 @@
 # File with constants
+URBAN_ROUTES_URL =''
+ADDRESS_FROM = 'East 2nd Street, 601'
+PHONE_NUMBER = '1300 1st St'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
--- main.py
+++ main.py
@@ -1,2 +1,41 @@
-
+import data
 class TestUrbanRoutes:
+    def test_set_route(self):
+        # Add in S8
+        print("function created for set route")
+        pass
+
+    def test_select_plan(self):
+        # Add in S8
+        print("function created for select plan")
+        pass
+
+    def test_fill_phone_number(self):
+        # Add in S8
+        print("function created for fill phone number")
+        pass
+
+    def test_fill_card_number(self):
+        # Add in S8
+        print("function created for fill card number")
+        pass
+
+    def test_comment_for_driver(self):
+        # Add in S8
+        print("function created for comment for driver")
+        pass
+
+    def test_order_blanket_and_hankerchiefs(self):
+        # Add in S8
+        print("function created for order blanket and hankerchiefs")
+        pass
+
+    def test_order_2_ice_cream(self):
+        # Add in S8
+        print("function created for order 2 ice cream")
+        pass
+
+    def test_car_search_model_appears(self):
+        # Add in S8
+        print("function created for car search model")
+        pass
